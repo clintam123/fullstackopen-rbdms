a49ebbbb9d4aee9216ac3e376a86e0cfb0373922
clintam123 <nguyenhanh1074@gmail.com>
2022-01-18 00:35:28 +0700

    13.10-13.12

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,15 +1,38 @@
 const router = require("express").Router();
+const jwt = require("jsonwebtoken");
 
-const { Blog } = require("../models");
+const { Blog, User } = require("../models");
+const { SECRET } = require("../util/config");
+
+const tokenExtractor = async (req, res, next) => {
+  const authorization = req.get("authorization");
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    try {
+      req.decodedToken = jwt.verify(authorization.substring(7), SECRET);
+    } catch {
+      res.status(401).json({ error: "token invalid" });
+    }
+  } else {
+    res.status(401).json({ error: "token missing" });
+  }
+  req.user = await User.findByPk(req.decodedToken.id);
+  next();
+};
 
 router.get("/", async (req, res) => {
-  const blogs = await Blog.findAll();
+  const blogs = await Blog.findAll({
+    attributes: { exclude: ["UserId"] },
+    include: {
+      model: User,
+      attributes: ["name"],
+    },
+  });
   res.json(blogs);
 });
 
-router.post("/", async (req, res) => {
-  const { title, author, url, likes } = req.body;
-  const blog = await Blog.create({ title, author, url, likes });
+router.post("/", tokenExtractor, async (req, res) => {
+  const user = req.user;
+  const blog = await Blog.create({ ...req.body, UserId: user.id });
   return res.json(blog);
 });
 
@@ -18,15 +41,25 @@ const blogFinder = async (req, res, next) => {
   next();
 };
 
-router.delete("/:id", blogFinder, async (req, res) => {
-  await req.blog.destroy();
-  res.status(204).end();
+router.delete("/:id", blogFinder, tokenExtractor, async (req, res) => {
+  const user = req.user;
+  if (user.id === req.blog.UserId) {
+    await req.blog.destroy();
+    res.status(204).end();
+  } else {
+    response.status(401).end();
+  }
 });
 
-router.put("/:id", blogFinder, async (req, res) => {
-  req.blog.likes = req.body.likes;
-  await req.blog.save();
-  res.json(req.blog);
+router.put("/:id", blogFinder, tokenExtractor, async (req, res) => {
+  const user = req.user;
+  if (user.id === req.blog.UserId) {
+    req.blog.likes = req.body.likes;
+    await req.blog.save();
+    res.json(req.blog);
+  } else {
+    response.status(401).end();
+  }
 });
 
 module.exports = router;
