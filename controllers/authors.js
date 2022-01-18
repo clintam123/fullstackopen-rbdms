43e4b1eed94129852dea6d6675204d17e9a8b606
clintam123 <nguyenhanh1074@gmail.com>
2022-01-18 13:23:46 +0700

    13.16

---
+++ controllers/authors.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+const sequelize = require("sequelize");
+
+const { Blog } = require("../models");
+
+router.get("/", async (req, res) => {
+  const authors = await Blog.findAll({
+    attributes: [
+      "author",
+      [sequelize.fn("count", sequelize.col("id")), "blogs"],
+      [sequelize.fn("sum", sequelize.col("likes")), "likes"],
+    ],
+    group: ["author"],
+    order: [[sequelize.fn("sum", sequelize.col("likes")), "DESC"]],
+  });
+  res.json(authors);
+});
+
+module.exports = router;
